047b5b94d8a3f2d4f3b59299c9001cce94d86dbf
Damien Sabouraud <damsbooster@hotmail.com>
2021-05-28 17:30:37 +0200

    test circleci

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production
 DEBUG = True
 
 ALLOWED_HOSTS = ['oc-lettings-3.herokuapp.com']
