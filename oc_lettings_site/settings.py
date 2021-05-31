a854918a55bfdb1b6dbc03b082719565b5873601
Damien Sabouraud <damsbooster@hotmail.com>
2021-05-31 10:53:37 +0200

    sentry + variable environnement

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -2,20 +2,18 @@ import os
 
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+import environ
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = ['127.0.0.1', 'oc-lettings-3.herokuapp.com']
 
@@ -116,7 +114,7 @@ STATIC_URL = '/static/'
 
 
 sentry_sdk.init(
-    dsn="https://fbfc78e7272d4c7eb1ef8f7a80d821b4@o761570.ingest.sentry.io/5793114",
+    dsn=env("SENTRY_DSN"),
     integrations=[DjangoIntegration()],
 
     # Set traces_sample_rate to 1.0 to capture 100%
